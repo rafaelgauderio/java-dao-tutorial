b88b719cb04930a2e59f53e19e9a6720bff4dc31
Rafael De Luca <deluca1712@gmail.com>
2019-12-25 23:51:52 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,23 @@
 package application;
 
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Locale.setDefault(Locale.US);
+
 		Department object = new Department(1, "Computers");
 		System.out.println(object);
+		System.out.println();
+
+		Seller seller = new Seller(15, "Rafael", "rafael@gmail.com", new Date(), 4500.00, object);
+		System.out.println(seller);
 
 	}
 

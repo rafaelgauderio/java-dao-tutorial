0ec71e3b237b172dbb3fba86a8d897de958d58bb
Rafael De Luca <deluca1712@gmail.com>
2019-12-25 21:47:05 -0300

    Department class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department object = new Department(1, "Computers");
+		System.out.println(object);
+
+	}
+
+}

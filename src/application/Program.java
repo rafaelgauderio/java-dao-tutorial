3dd2e43567b431f41189dfd9190f3b39572a9e9a
Rafael De Luca <deluca1712@gmail.com>
2019-12-27 15:37:09 -0300

    DaoFactory and SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Date;
 import java.util.Locale;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,7 +17,9 @@ public class Program {
 		Department object = new Department(1, "Computers");
 		System.out.println(object);
 		System.out.println();
-
+		
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = new Seller(15, "Rafael", "rafael@gmail.com", new Date(), 4500.00, object);
 		System.out.println(seller);
 

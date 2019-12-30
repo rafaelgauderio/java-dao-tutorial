bb269adf90cc07c1ad1a5c0431c8d66c453e87b5
Rafael De Luca <deluca1712@gmail.com>
2019-12-30 14:11:36 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,9 @@
 package application;
 
-import java.sql.PreparedStatement;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,7 +13,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
 
 		Department object = new Department(1, "Computers");
@@ -55,6 +56,14 @@ public class Program {
 		System.out.println("Update complete");
 		Seller sellerFind2 = sellerDao.findById(41);
 		System.out.println(sellerFind2);
+		
+		System.out.println("\n===TEST six: selle DeletebyId===");
+		System.out.println("Inform a id to delete data");
+		int idDel = sc.nextInt();
+		sellerDao.deleteById(idDel);
+		System.out.println("Delete complete");		
+		sc.close();
+		
 
 	}
 
